c254c6b747c87a0aa7ea3081353d7d5fcc725f93
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-06-28 10:09:42 +0100

    🎉 First commit

---
+++ src/main/java/com/alex/dev/api_design_pattern/ApiDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.alex.dev.api_design_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/dev/api_design_pattern/ApiDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.dev.api_design_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
